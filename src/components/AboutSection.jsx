e6f289d23a85207a6c0472365878d4748256836a
sifaq00 <asysyifaq25@gmail.com>
2026-01-12 18:36:15 +0700

    update download cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="/cv.pdf"
+                href="assets/cv.pdf"
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300 flex items-center justify-center"
               >
